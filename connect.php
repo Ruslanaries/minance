8e8731a891ce47f61cbfd1eec9b7a615475e27eb
Ruslanaries <ruslanaries@bk.ru>
2025-06-11 18:54:32 +0400

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,16 +1,13 @@
+<?php
+  $host = 'localhost';  // Хост, у нас все локально
+  $user = 'minance_usr';    // Имя созданного вами пользователя
+  $pass = 'Hu8XuhfhAjTve792'; // Установленный вами пароль пользователю
+  $db_name = 'minance';   // Имя базы данных
+  $link = mysqli_connect($host, $user, $pass, $db_name); // Соединяемся с базой
 
-Cloning into 'C:\Users\HP\Downloads\minance\aaa\minance'...
-remote: Enumerating objects: 15, done.        
-remote: Counting objects: 100% (15/15), done.        
-remote: Compressing objects: 100% (14/14), done.        
-remote: Total 15 (delta 1), reused 0 (delta 0), pack-reused 0 (from 0)        
-Receiving objects: 100% (15/15), 493.59 KiB | 1.29 MiB/s, done.
-Resolving deltas: 100% (1/1), done.
-error: invalid path 'con.php'
-fatal: unable to checkout working tree
-warning: Clone succeeded, but checkout failed.
-You can inspect what was checked out with 'git status'
-and retry with 'git restore --source=HEAD :/'
-
-
-Would you like to retry cloning ?
+  // Ругаемся, если соединение установить не удалось
+  if (!$link) {
+    echo 'Код ошибки: ' . mysqli_connect_errno() . ', ошибка: ' . mysqli_connect_error();
+    exit;
+  }
+?>
