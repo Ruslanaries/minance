392c19af8015723aeb236a678ef8a9e479f732c2
Ruslanaries <ruslanaries@bk.ru>
2025-06-11 18:54:12 +0400

    Update and rename con.php to connect.php

---
+++ connect.php
@@ -0,0 +1,16 @@
+
+Cloning into 'C:\Users\HP\Downloads\minance\aaa\minance'...
+remote: Enumerating objects: 15, done.        
+remote: Counting objects: 100% (15/15), done.        
+remote: Compressing objects: 100% (14/14), done.        
+remote: Total 15 (delta 1), reused 0 (delta 0), pack-reused 0 (from 0)        
+Receiving objects: 100% (15/15), 493.59 KiB | 1.29 MiB/s, done.
+Resolving deltas: 100% (1/1), done.
+error: invalid path 'con.php'
+fatal: unable to checkout working tree
+warning: Clone succeeded, but checkout failed.
+You can inspect what was checked out with 'git status'
+and retry with 'git restore --source=HEAD :/'
+
+
+Would you like to retry cloning ?
